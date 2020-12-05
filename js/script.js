eac09de68f86cf595a849b9e7b2022f989b7eb9b
Madiyar <madiyaresmuhambetov7@gmail.com>
2020-12-05 18:56:48 +0600

    calculator

--- js/script.js
+++ js/script.js
@@ -169,6 +169,42 @@ let slideIndex = 1,
             }
         }
     });
+    //calc
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        plays = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+        totalValue.innerHTML = '0';
+        persons.addEventListener('change', function(event){
+            personsSum = +this.value;
+            total = (daysSum + personsSum)*4000;
+            if(restDays.value == '' || personsSum.value == ''){
+                totalValue.innerHTML = '0';
+            }else{
+                totalValue.innerHTML = total;
+            }
+        });
+        restDays.addEventListener('change', function(event){
+            daysSum = +this.value;
+            total = (daysSum + personsSum)*4000;
+            if(restDays.value == '' || personsSum.value == ''){
+                totalValue.innerHTML = '0';
+            }else{
+                totalValue.innerHTML = total;
+            }
+        });
+        plays.addEventListener('change', function(){
+            if(restDays.value == '' || personsSum.value == '' ){
+                totalValue.innerHTML = total; 
+            }else{
+                let a = total;
+                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            }
+        });
+        
 });
 
 
